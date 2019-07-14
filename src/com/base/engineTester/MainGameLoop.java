8dfd8c50f4256df844047183b105c44d7d763d26
Gvtn <Gamevtn@outlook.com>
2019-07-14 11:50:08 +0800

    update code 0.18.6

--- src/com/base/engineTester/MainGameLoop.java
+++ src/com/base/engineTester/MainGameLoop.java
@@ -3,9 +3,12 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import models.RawModel;
+import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -15,6 +18,7 @@ public class MainGameLoop {
 
         Loader loader = new Loader();
         Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
 
 
         float[] vertices = { // vbo
@@ -32,15 +36,28 @@ public class MainGameLoop {
                 3,1,2 // bottom right triangle (v3 v1 v2)
         };
 
-        RawModel model = loader.loadToVAO(vertices, indices);
+        float[] textureCoords = {
+                0,0,
+                0,1,
+                1,1,
+                1,0
+        };
+
+        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+        TexturedModel texturedModel = new TexturedModel(model, texture);
 
         while(!Display.isCloseRequested()) {
             renderer.prepare();
             //game logic
-            renderer.render(model);
+
+            shader.start();
+            renderer.render(texturedModel);
+            shader.stop();
             DisplayManager.updateDisplay();
         }
 
+        shader.cleanUp();
         loader.cleanUp();
 
         DisplayManager.closeDisplay();
