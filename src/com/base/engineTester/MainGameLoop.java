b60e7d06ee5c05a52851585621ce00cca4b466b3
Gvtn <Gamevtn@outlook.com>
2019-07-28 10:03:23 +0800

    my terrain has reflectivity and damper variables, both in the java and shader code

--- src/com/base/engineTester/MainGameLoop.java
+++ src/com/base/engineTester/MainGameLoop.java
@@ -16,6 +16,7 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
+import terrains.Terrain;
 import textures.ModelTexture;
 
 public class MainGameLoop {
@@ -23,42 +24,45 @@ public class MainGameLoop {
     public static void main(String[] args) {
 
         DisplayManager.createDisplay();
-
         Loader loader = new Loader();
 
-        RawModel model = OBJLoader.loadObjModel("dragon", loader);
+        RawModel model = OBJLoader.loadObjModel("tree", loader);
 
-        TexturedModel cubeModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("white")));
-        ModelTexture texture = cubeModel.getTexture();
+        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("tree")));
+        ModelTexture texture = staticModel.getTexture();
         texture.setShineDamper(10);
         texture.setReflectivity(1);
 
-        Light light = new Light(new Vector3f(3000, 2000, 3000), new Vector3f(1,1,1));
-        Camera camera = new Camera();
-
-        List<Entity> allCubes = new ArrayList<Entity>();
+        List<Entity> entities = new ArrayList<Entity>();
         Random random = new Random();
 
-        for (int i = 0; i < 200; i++){
-            float x = random.nextFloat() * 100 -50;
-            float y = random.nextFloat() * 100 -50;
-            float z = random.nextFloat() * -300;
-            allCubes.add(new Entity(cubeModel, new Vector3f(x,y,z),
-                    random.nextFloat() * 180f,
-                    random.nextFloat() * 180f,
-                    random.nextFloat() * 0f,
-                    1f));
+        for (int i = 0; i < 500; i++){
+            entities.add(new Entity(staticModel, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,3));
         }
+        Light light = new Light(new Vector3f(20000, 20000, 2000), new Vector3f(1,1,1));
+
+        Terrain terrain = new Terrain(0,0,loader,new ModelTexture(loader.loadTexture("grass")));
+        Terrain terrain2 = new Terrain(1,0,loader,new ModelTexture(loader.loadTexture("grass")));
+        terrain.getTexture().setShineDamper(10);
+        terrain.getTexture().setReflectivity(1);
+        terrain2.getTexture().setShineDamper(10);
+        terrain2.getTexture().setReflectivity(1);
+
+        Camera camera = new Camera();
+
+
 
         MasterRenderer renderer = new MasterRenderer();
 
         while(!Display.isCloseRequested()) {
             //entity.increaseRotation(0, 1, 0);
             camera.move();
+            renderer.processTerrain(terrain);
+            renderer.processTerrain(terrain2);
 
-            for (Entity cube : allCubes){
-                cube.move();// I added this function, it does not exist in the tutorial
-                renderer.processEntity(cube);
+            for (Entity entity : entities){
+                entity.move();// I added this function, it does not exist in the tutorial
+                renderer.processEntity(entity);
             }
             renderer.render(light, camera);
             DisplayManager.updateDisplay();
